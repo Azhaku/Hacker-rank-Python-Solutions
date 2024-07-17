e84dccd5ecaa65206942c076a7572ffa75ea7944
Azhaku <77579299+Azhaku@users.noreply.github.com>
2024-07-17 20:19:00 +0530

    Add files via upload

---
+++ MountBlue Tech/17. Divisible Sum Pairs.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    dic= {}
+    res = 0
+    for i in ar:
+        mod = (i%k)
+        rem = k - mod if mod !=0 else 0
+        if rem in dic:
+            res += dic[rem]
+        if mod not in dic:
+            dic[mod] = 1
+        else:
+            dic[mod] +=1
+    return res
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
